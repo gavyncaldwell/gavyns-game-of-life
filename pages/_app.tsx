a33deed65cc463cdc241ae72e930e978430490ef
Gavyn Caldwell <gavyncaldwell@icloud.com>
2023-03-22 04:09:44 -0600

    added font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import { Normalize } from 'styled-normalize'
+import '@style/global.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
